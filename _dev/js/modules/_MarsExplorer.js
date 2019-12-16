49bddbb0544158c281edaf13c154dbb599ae0697
Koganas <danielsank@gmail.com>
2019-12-16 05:26:36 -0300

    introduce MarsExplorer UI: createPlateau() and paintBlock()

--- _dev/js/modules/_MarsExplorer.js
+++ _dev/js/modules/_MarsExplorer.js
@@ -13,6 +13,9 @@ class MarsExplorer {
 
     this.checkPosition = this.checkPosition.bind(this);
     this.execCommand = this.execCommand.bind(this);
+
+    this.map = document.querySelector('.map'); 
+    this.createPlateau();
   }
 
   /* Check for borders and rover collisions. */
@@ -51,9 +54,15 @@ class MarsExplorer {
     const rover = new Rover(x, y, compass);
     this.rovers.push(rover);
     this.activeRover = rover;
+    this.paintBlock(x, y, compass, this.rovers.length);
 
     // Index number acts as the Rover ID.
     return this.rovers.length - 1;
+  }
+
+  /* Activate rover by ID. */
+  activateRover(index) {
+    return !!(this.activeRover = this.rovers[index]);
   }
   
   /* Execute command. */
@@ -71,27 +80,96 @@ class MarsExplorer {
   }
 
   /* Convert command string to array and send each command to active rover. */
-  sendCommand(commandList) {
+  sendCommand(commandList, id) {    
     let cmd = [...commandList],
         run = setInterval(()=>{
-                this.getCurrentPosition()
+                this.getCurrentPosition(id)
                 console.log(cmd[0])
                 this.execCommand(cmd.shift())
                 if (cmd.length === 0) {                  
                   clearInterval(run)
-                  setTimeout(()=>this.getCurrentPosition(), 800)
+                  setTimeout(()=>this.getCurrentPosition(id), 800)
                 }
               }, 800);
+
+/*
+    [...commandList].map((cmd, i, array)=>{
+      const arraySize = array.length - 1;
+      let run;
+
+      this.execCommand(cmd)
+      if(i === 0) {
+        this.getCurrentPosition()
+      } else if(i < arraySize && i != 0) {
+        run = setInterval(()=>{
+          this.getCurrentPosition()
+        }, 1000);
+      } else {
+        clearInterval(run);
+      }
+      
+    })
+*/
+
   }
   
   /* Return array of rover final positions. */
-  getCurrentPosition() {
+  getCurrentPosition(id) {
     return this.rovers.map(rover => {
       const state = rover.getState();
       let xPos = (state.pos.x===0) ? 1 : state.pos.x,
           yPos = (state.pos.y===0) ? 1 : state.pos.y;
-      console.log(xPos, yPos, state.compass);
+      this.paintBlock(xPos, yPos, state.compass, id);
       return `${xPos} ${yPos} ${state.compass}`;
+    });
+  }
+
+  /* UI - Create plateau grid */
+  createPlateau() {
+    let x = this.size.x,
+        y = this.size.y;    
+    this.map.innerHTML = '';
+    for (let i = x; i > 0; i--) {
+      let tr = document.createElement('tr');
+      for (let _i = y; _i > 0; _i--) {
+        tr.insertAdjacentHTML('afterbegin', '<td class="map__block"></td>');
+      }
+      this.map.insertAdjacentElement('afterbegin', tr);
+    }
+  }
+
+  /* UI - Paint position */
+  paintBlock(x, y, compass, rover) {
+    let xLength = this.size.x,
+        yLength = this.size.y;
+    console.log(rover);
+    this.activateRover(rover);
+    this.map.querySelectorAll('.R'+rover).forEach( e=>e.className = 'map__block' );
+    this.map.querySelectorAll('.map tr').forEach((e, i)=>{
+      let yPos = this.size.y - i;
+      if(y==yPos) {
+        e.querySelectorAll('td').forEach((elm,i)=>{
+          let xPos = i + 1;
+          if(x==xPos) {
+            console.log(xPos, yPos, compass);
+            elm.className = '';
+            switch (compass) {
+              case 'N':
+                elm.classList.add('R'+rover, 'rover', 'north')
+                break;
+              case 'S':
+                elm.classList.add('R'+rover, 'rover', 'south')
+                break;
+              case 'W':
+                elm.classList.add('R'+rover, 'rover', 'west')
+                break;
+              case 'E':
+                elm.classList.add('R'+rover, 'rover', 'east')
+                break;
+            }
+          }
+        });
+      }      
     });
   }
 
--- _dev/js/templates/script.js
+++ _dev/js/templates/script.js
@@ -3,13 +3,13 @@ import MarsExplorer from '../modules/_MarsExplorer';
 (() => {
   const core = {
     init() {
-      this.cacheDOM();
+    	this.cacheDOM();
     },
 
     cacheDOM() {
       const marsExplorer = new MarsExplorer(5, 5);
       marsExplorer.addRover(1, 2, 'N');
-      marsExplorer.sendCommand('LMLMLMLMM');
+      marsExplorer.sendCommand('LMLMLMLMM', 0);
     }
   };
   core.init();

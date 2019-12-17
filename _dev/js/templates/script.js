6df4a0c97fa6bb225bad1d80a72098b87a04046b
Koganas <danielsank@gmail.com>
2019-12-17 08:55:46 -0300

    MarsExplorer UI: addRover()  and newExplorer()

--- _dev/js/templates/script.js
+++ _dev/js/templates/script.js
@@ -3,17 +3,34 @@ import MarsExplorer from '../modules/_MarsExplorer';
 (() => {
   const core = {
     init() {
-    	this.cacheDOM();
+      this.cacheDOM();
     },
 
     cacheDOM() {
-      const marsExplorer = new MarsExplorer(5, 5);
+      this.controlArea = document.querySelector('.control-area');
+      this.button = document.querySelector('#btnAdd');
+      this.button.addEventListener('click', () => {
+        const xSize = document.querySelector('input[name="xSize"]').value,
+          ySize = document.querySelector('input[name="ySize"]').value,
+          x = document.querySelector('input[name="xPos"]').value,
+          y = document.querySelector('input[name="yPos"]').value,
+          compass = document.querySelector('input[name="compass"]').value,
+          command = document.querySelector('input[name="command"]').value;
+        this.controlArea.innerHTML = '';
+        //console.log(Number(xSize), Number(ySize), Number(x), Number(y), compass, command);
+        this.addRover(Number(xSize), Number(ySize), Number(x), Number(y), compass, command);
+      });
+    },
 
-      marsExplorer.addRover(1, 2, 'N');
-      marsExplorer.sendCommand('LMLMLMLMM');
+    newExplorer(xSize, ySize) {
+      const newExplorer = new MarsExplorer(xSize, ySize);
+      return newExplorer;
+    },
 
-/*    marsExplorer.addRover(3, 3, 'E');
-      marsExplorer.sendCommand('MMRMMRMRRM');*/
+    addRover(xSize, ySize, x, y, compass, command) {
+      const marsExplorer = this.newExplorer(xSize, ySize);
+      marsExplorer.addRover(x, y, compass);
+      marsExplorer.sendCommand(command);
     }
   };
   core.init();

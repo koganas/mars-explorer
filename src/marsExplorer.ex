bcc6d8bb980ce1af7e6120f514dce940f9ae9080
Koganas <danielsank@gmail.com>
2019-12-19 03:05:50 -0300

    testing IO

--- src/marsExplorer.ex
+++ src/marsExplorer.ex
@@ -20,6 +20,36 @@ defmodule MarsExplorer do
   def new_position(<<position::binary-size(2), compass::binary-size(1)>> = current_position, "M"),
           do: move(current_position, compass)
 
+  # def rotate_left(position, compass) do
+  #   if compass = "N" do
+  #     position <> "W"
+  #   end
+  #   if compass = "E" do
+  #     position <> "N"
+  #   end
+  #   if compass = "W" do
+  #     position <> "S"
+  #   end
+  #   if compass = "S" do
+  #     position <> "E"
+  #   end
+  # end
+
+  # def rotate_right(position, compass) do
+  #   if compass = "N" do
+  #     position <> "E"
+  #   end
+  #   if compass = "E" do
+  #     position <> "S"
+  #   end
+  #   if compass = "W" do
+  #     position <> "N"
+  #   end
+  #   if compass = "S" do
+  #     position <> "W"
+  #   end
+  # end
+
   # Rotate rover based on compass
   def rotate_left(position, "N"), do: position <> "W"
   def rotate_left(position, "E"), do: position <> "N"
@@ -36,10 +66,13 @@ defmodule MarsExplorer do
   def move(<<x_position::binary-size(1), y_position::binary-size(1), compass::binary-size(1)>>, "W"), do: dec(x_position) <> y_position <> compass
   def move(<<x_position::binary-size(1), y_position::binary-size(1), compass::binary-size(1)>>, "S"), do: x_position <> dec(y_position) <> compass
 
+  # def IO.gets("What's the size of the area you want to explore?")
+
   # Return rover final position
   defp inc(number), do: Integer.to_string(String.to_integer(number) + 1)
   defp dec(number), do: Integer.to_string(String.to_integer(number) - 1)
 
 end
+
 
 MarsExplorer.execute([5,5], [["12N", "LMLMLMLMM"], ["33E", "MMRMMRMRRM"]])
